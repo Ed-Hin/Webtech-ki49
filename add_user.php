cb9de956459b32c25f8feeb5a8d80c67d8fb730b
shilana <shilana.rajabi.haji.khosh@student.uva.nl>
2023-01-19 14:39:30 +0100

    add_user aangepast

--- add_user.php
+++ add_user.php
@@ -1,12 +1,13 @@
 <?php
 include '../../connection.php';
 
+$name = mysqli_real_escape_string($connection, htmlspecialchars($_POST["name"]));
 $email = mysqli_real_escape_string($connection, htmlspecialchars($_POST["email"]));
-$user = mysqli_real_escape_string($connection, htmlspecialchars($_POST["user"]));
-$pass = mysqli_real_escape_string($connection, htmlspecialchars($_POST["pass"]));
+$user = mysqli_real_escape_string($connection, htmlspecialchars($_POST["username"]));
+$pass = mysqli_real_escape_string($connection, htmlspecialchars($_POST["password"]));
 
 try {
-$sql = "INSERT INTO Users (user, email, pass) VALUES ('$user', '$email', '$pass')";
+$sql = "INSERT INTO Users (name, user, email, pass) VALUES ('$name', '$user', '$email', '$pass')";
 mysqli_query($connection, $sql);
 } catch(PDOExeption $e) {
     echo $sql . "<br>" . $e->getMessage();
