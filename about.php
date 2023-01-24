c178dac0f4d78df5c77d41a73e45ebb787a0cc14
shilana <shilana.rajabi.haji.khosh@student.uva.nl>
2023-01-24 10:44:18 +0100

    Moved header to every file

---
+++ about.php
@@ -0,0 +1,10 @@
+<?php
+include "header.php";
+?>
+    <br><br><br><br><br><br><br><br><br><br><br>
+    
+    <h2>About WhoAsked</h2>
+        
+    <p>WhoAsked is a newly created discussion forum founded in 2023 by Edwin Cheng, Shilana Rajabi Haji Khosh, Simon Bevaart and Sil Wilderink at the University of Amsterdam. The aim of WhoAsked is to give a platform to people who like to share their opinion and discuss with other people in the community about a variety of topics.</p>
+    </body>
+</html>
--- header.php
+++ header.php
@@ -15,13 +15,13 @@ session_start();
             <img src="https://cdn.discordapp.com/attachments/760819935751438376/1065750623917711540/logo_white.png" class="logo" alt="WhoAsked">
                 <ul class="navbar">
                     <li><a href="index.php" class="active">Home</a></li>
-                    <li><a href="popular.html">Popular</a></li>
-                    <li><a href="topics.html">Topics</a></li>
-                    <li><a href="about.html">About</a></li>
+                    <li><a href="popular.php">Popular</a></li>
+                    <li><a href="topics.php">Topics</a></li>
+                    <li><a href="about.php">About</a></li>
                 </ul>
             <div class="personal">
                 <a href="login.php" class="user">Login</a>
-                <a href="register.html">Register</a>
+                <a href="register.php">Register</a>
             </div>
         </header>
     </body>
