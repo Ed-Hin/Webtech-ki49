0304ced02879d79cd6e7cb3830620d59c757b63f
shilana <shilana.rajabi.haji.khosh@student.uva.nl>
2023-01-24 10:21:15 +0100

    header.php gemaakt

---
+++ header.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="home.css">
+    <title>WhoAsked</title>
+    <link rel="icon" type="image/x-icon" href="logo_alleen_symbol.png">
+</head>
+    <body>
+        <header>
+            <img src="https://cdn.discordapp.com/attachments/760819935751438376/1065750623917711540/logo_white.png" class="logo" alt="WhoAsked">
+                <ul class="navbar">
+                    <li><a href="home.html" class="active">Home</a></li>
+                    <li><a href="popular.html">Popular</a></li>
+                    <li><a href="topics.html">Topics</a></li>
+                    <li><a href="about.html">About</a></li>
+                </ul>
+            <div class="personal">
+                <a href="login.html" class="user">Login</a>
+                <a href="register.html">Register</a>
+            </div>
+        </header>
+    </body>
+</html>
