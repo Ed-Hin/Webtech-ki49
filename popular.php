23a4b5c77ec44ca17ac85553922ce9c43f0b1f90
Simon <simon.bevaart@student.uva.nl>
2023-01-25 16:19:06 +0100

    blblaba

---
+++ popular.php
@@ -0,0 +1,21 @@
+<?php
+include "header.php";
+?>
+
+<head>
+<link rel="stylesheet" href="popular.css">
+<head>
+<br>
+<p>When was it popular?</p>
+<!--Filter Box-->
+<div class="filter-box">
+    <div>
+        <select name="" id="">
+            <option value="Today">Today</option>
+            <option value="This week">This week</option>
+            <option value="This month">This month</option>
+            <option value="This year">This year</option>
+            <option value="All time">All time</option>
+        </select>
+    </div>
+</div>
