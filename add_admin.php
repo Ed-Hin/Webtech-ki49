5025bda27e8ba11ad28ab2bf3681699c10b27951
shilana <shilana.rajabi.haji.khosh@student.uva.nl>
2023-01-25 19:41:55 +0100

    admin php aangemaakt

---
+++ add_admin.php
@@ -0,0 +1,16 @@
+<?php
+include '../../connection.php';
+
+$name = mysqli_real_escape_string($connection, htmlspecialchars($_POST["name"]));
+$email = mysqli_real_escape_string($connection, htmlspecialchars($_POST["email"]));
+$user = mysqli_real_escape_string($connection, htmlspecialchars($_POST["username"]));
+$pass = mysqli_real_escape_string($connection, htmlspecialchars($_POST["password"]));
+$hash = password_hash($pass, PASSWORD_DEFAULT);
+
+try {
+$sql = "INSERT INTO Admins (name, user, email, pass) VALUES ('$name', '$user', '$email', '$hash')";
+mysqli_query($connection, $sql);
+} catch(PDOExeption $e) {
+    echo $sql . "<br>" . $e->getMessage();
+}
+?>
