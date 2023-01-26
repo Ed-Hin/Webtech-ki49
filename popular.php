abefd6779f161c2bbb2567fb9cda941a850d5998
Simon <simon.bevaart@student.uva.nl>
2023-01-26 14:44:55 +0100

    Popular.php aangepast

--- popular.php
+++ popular.php
@@ -1,19 +1,41 @@
 <?php
 include "header.php";
 ?>
-<br><br><br><br><br><br><br><br>
-            <!--Filter Box-->
-            <div class="filter-box">
-                <div>
-                    <select name="" id="">
-                        <option value="Last hour">Last hour</option>
-                        <option value="Today">Today</option>
-                        <option value="This week">This week</option>
-                        <option value="This month">This month</option>
-                        <option value="This year">This year</option>
-                        <option value="All time">All time</option>
-                    </select>
-                    
+<link rel="stylesheet" href="popular.css">
+<br><br>
+            
+            <br><br>
+            <div class="posts">
+                <h1> Popular posts</h1>
+                <!--Filter Box-->
+                <div class="filter-box">
+                    <div>
+                        <select name="" id="">
+                            <option value="Last hour">Last hour</option>
+                            <option value="Today">Today</option>
+                            <option value="This week">This week</option>
+                            <option value="This month">This month</option>
+                            <option value="This year">This year</option>
+                            <option value="All time">All time</option>
+                        </select>
+                    </div>
+                </div>
+                <br>
+                <div class="popular-post">
+                    <div class="container">
+                        <h2>Ik vind dit echt vet</h2>
+                        <br>
+                        <p>jatoch</p>
+                        <h3>Likes | Published on: ../../....</h3> 
+                    </div>
+                </div>
+                <div class="popular-post2">
+                    <div class="container2">
+                        <h2>Ik vind dit echt vet</h2>
+                        <br>
+                        <p>jatoch</p>
+                        <h3>Likes | Published on: ../../....</h3>
+                    </div>
                 </div>
             </div>
     </body>
