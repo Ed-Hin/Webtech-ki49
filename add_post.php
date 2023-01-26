7b30488803070bf9ca2b818ba64c759d26a6be0b
shilana <shilana.rajabi.haji.khosh@student.uva.nl>
2023-01-26 13:45:42 +0100

    post php & comment

--- add_post.php
+++ add_post.php
@@ -1,16 +1,12 @@
 <?php
 include '../../connection.php';
 
-$user = mysqli_real_escape_string($connection, htmlspecialchars($_POST["user"]));
-$datetime = mysqli_real_escape_string($connection, htmlspecialchars($_POST["datetime"]));
 $category = mysqli_real_escape_string($connection, htmlspecialchars($_POST["category"]));
 $title = mysqli_real_escape_string($connection, htmlspecialchars($_POST["title"]));
 $contents = mysqli_real_escape_string($connection, htmlspecialchars($_POST["contents"]));
-$likes = mysqli_real_escape_string($connection, htmlspecialchars($_POST["likes"]));
-$comments = mysqli_real_escape_string($connection, htmlspecialchars($_POST["comments"]));
 
 try {
-$sql = "INSERT INTO Admins (user, datetime, category, title, contents, likes, comments) VALUES ('$user', '$datetime', '$category', '$title', '$contents', '$likes', '$comments')";
+$sql = "INSERT INTO Posts (category, title, contents) VALUES ('$category', '$title', '$contents')";
 mysqli_query($connection, $sql);
 } catch(PDOExeption $e) {
     echo $sql . "<br>" . $e->getMessage();
