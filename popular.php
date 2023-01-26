5272cabd79524aa9e6859e6db0774a6cfa2cbf30
Simon <simon.bevaart@student.uva.nl>
2023-01-26 12:03:06 +0100

    Ik heb alles gepushd

--- popular.php
+++ popular.php
@@ -1,3 +1,20 @@
 <?php
 include "header.php";
 ?>
+<br><br><br><br><br><br><br><br>
+            <!--Filter Box-->
+            <div class="filter-box">
+                <div>
+                    <select name="" id="">
+                        <option value="Last hour">Last hour</option>
+                        <option value="Today">Today</option>
+                        <option value="This week">This week</option>
+                        <option value="This month">This month</option>
+                        <option value="This year">This year</option>
+                        <option value="All time">All time</option>
+                    </select>
+                    
+                </div>
+            </div>
+    </body>
+</html>
