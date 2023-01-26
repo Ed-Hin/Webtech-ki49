bf129b62bfd3a660e25fa62e4aff33cb4304f96b
Edwin <edwincheng619@gmail.com>
2023-01-26 22:39:42 +0100

    Nieuwe index.php erin

--- index.php
+++ index.php
@@ -5,32 +5,27 @@ include "footer.php";
 
 <div class="posts">
     <h1>Latest posts</h1>
+    <br>
     <div class="post">
-        <a href="posts.php"><div class="container">
-            <h2>Discussion</h2>
-            <br>
-            <p>blablabla...</p>
-            <br>
-            <p>Posted by ...</p>
-            <p>Time...</p>
-        </div>
+        <a href="posts.php">
+            <div class="container">
+                <h2>Ik vind dit echt vet</h2>
+                <p>jatoch</p>
+                <h3>10000 Likes | Published on: ../../....</h3>
+            </div>
         </a>
     </div>
     <br>
     <a href="posts.php">
-    <div class="post2">
-        <div class="container2">
-            <h2>Discussion</h2>
-            <br>
-            <p>blablabla...</p>
-            <br>
-            <p>Posted by ...</p>
-            <p>Time...</p>
-            <!-- <i class='far fa-thumbs-up' style='font-size:48px;color:red'></i> -->
+        <div class="post2">
+            <div class="container2">
+                <h2>Ik vind dit echt vet</h2>
+                <p>jatoch</p>
+                <h3>10000 Likes | Published on: ../../....</h3>
+            </div>
         </div>
+    </a>
+    <div class="HomeImage">
+        <img src="interaction.png" alt="interaction" width="400" height="400">
     </div>
-</a>
-<div class="HomeImage">
-    <img src="interaction.png" alt="interaction" width="400" height="400">
-</div>
 </div>
