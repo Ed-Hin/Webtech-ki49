657c750b4351888002a11cedd038c2a3a9ea4805
Edwin <edwincheng619@gmail.com>
2023-01-31 15:44:14 +0100

    finished topics

--- topics.php
+++ topics.php
@@ -1,10 +1,9 @@
 <?php
 include "header.php";
 include("../../connection.php");
-    
+
     if (array_key_exists("login_user", $_SESSION)) {
         $userId = $_SESSION['user_id'];
-    }
     
     $sql = "SELECT * FROM `Posts` JOIN `Users` u on Posts.user = u.ID";
     $result = mysqli_query($connection,$sql);
@@ -12,10 +11,11 @@ include("../../connection.php");
     if($_SERVER["REQUEST_METHOD"] == "POST") { 
         if(isset($_POST['topics']) && $_POST['topics'] !=='Select') {
             $category = $_POST['topics'];
-            $sql = "SELECT * FROM Posts where category = '$category'";
+            $sql = "SELECT * FROM Posts JOIN `Users`u on Posts.user = u.ID where category = '$category'";
             $result = mysqli_query($connection, $sql);
         }
     }
+    
 ?>
 
 <link rel="stylesheet" href="topics.css">
@@ -25,7 +25,7 @@ include("../../connection.php");
     <form action="topics.php" method="post">
         <div class="filter-box">
             <select name="topics" id="topicslist">
-                <option value="Select" id="Selecttopic">Select topic..</option>
+                <option value="Select" id="Selecttopic">All topics</option>
                 <option value="Cars">Cars</option>
                 <option value="Pets">Pets</option>
                 <option value="Food">Food</option>
@@ -39,12 +39,12 @@ include("../../connection.php");
         <input type="submit" name="submit" value="submit">
     </form>
 
-<?php
+    <?php
     while($posts = $result->fetch_assoc()) { ?>
     <div class="post_info">
         <a href="posts.php">
             <div class="container">
-
+                <a href="user_post.php?id=<?php echo $posts['postid'];?>&username=<?php echo $posts['user'];?>">
                 <h2><?php echo $posts['title'] ?></h2>
                 <p><?php echo $posts['contents'] ?></p>
                 <p><?php echo $posts['category'] ?></p>
@@ -55,9 +55,22 @@ include("../../connection.php");
             </div>
         </a>
     </div>
-    <?php } ?>
-</body>
+    <?php } } else { ?>
+    <div class="mainpage">
+        <div class="txt">
+            <h1 class="welcome">Welcome to<br>WhoAsked!</h1>
+            <h2 class="sign">Sign up now!</h2>
+        </div>
+        <div class="HomeImage">
+            <img src="interaction.png" alt="interaction" class="interaction" width="550" height="550">
+        </div>
+    </div>
 
+
+
+    <?php     } ?>
+    <div class="clear"></div>
+</body>
 <?php
 include "footer.php";
 ?>
