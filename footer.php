0fcca6e49c0c3243008a20d1d00016009b508de7
Edwin <edwincheng619@gmail.com>
2023-02-01 22:46:03 +0100

    Styling

--- footer.php
+++ footer.php
@@ -1,80 +1,77 @@
-<!DOCTYPE html>
-<html lang="eng">
-
 <head>
     <link rel="stylesheet" href="footer.css">
 </head>
 <?php if (array_key_exists("login_user", $_SESSION)) { ?>
-    <footer class="footer" style="position:sticky">
-        <div class="footer_container">
-            <div class="footer-left">
-                <img src="logo_white_small.png" class="logo">
-                <p>Communicate with others through our forum!</p>
-            </div>
-    <ul class="footer-right">
-        <li>
-            <h2 class="footer-title">Explore</h2>
-            <ul class="explore">
-                <li><a href="popular.php">Popular</a></li>
-                <li><a href="topics.php">Topics</a></li>
-            </ul>
-        </li>
-
-        <li>
-            <h2 class="footer-title">Legal</h2>
-            <ul class="legal">
-                <li><a href="privacy.php">Privacy Policy</a></li>
-            </ul>
-        </li>
-
-        <li>
-            <h2 class="footer-title">Contact</h2>
-            <ul class="about">
-                <li>+31 (0)20 525 1401</li>
-                <li>
-                    <p>Science Park 904 1098 XH Amsterdam</p>
-                </li>
-                <li>
-                    <p>servicedesk-fs@uva.nl</p>
-                </li>
-        </li>
-    </ul>
-    </div>
-    <div class="footer-bottom">
-        <p>&copy; WhoAsked 2023</p>
-    </div>
-    </footer>
-<html>
-<?php
-    } else {
-?>
-<footer>
+<footer class="footer">
     <div class="footer_container">
         <div class="footer-left">
             <img src="logo_white_small.png" class="logo">
-            <p>Communicate with others through our forum!</p>
+            <p class="footer_text">Communicate with others through our forum!</p>
         </div>
         <ul class="footer-right">
             <li>
                 <h2 class="footer-title">Explore</h2>
                 <ul class="explore">
-                    <li><a href="popular.php">Popular</a></li>
-                    <li><a href="topics.php">Topics</a></li>
+                    <li><a href="popular.php" class="footer_text">Popular</a></li>
+                    <li><a href="topics.php" class="footer_text">Topics</a></li>
                 </ul>
             </li>
 
             <li>
                 <h2 class="footer-title">Legal</h2>
                 <ul class="legal">
-                    <li><a href="privacy.php">Privacy Policy</a></li>
+                    <li><a href="privacy.php" class="footer_text">Privacy Policy</a></li>
+                </ul>
+            </li>
+
+            <li>
+                <h2 class="footer-title">Contact</h2>
+                <ul class="about">
+                    <li>+31 (0)20 525 1401</li>
+                    <li>
+                        <p class="footer_text">Science Park 904 1098 XH Amsterdam</p>
+                    </li>
+                    <li>
+                        <p class="footer_text">servicedesk-fs@uva.nl</p>
+                    </li>
+            </li>
+        </ul>
+    </div>
+    <div class="footer-bottom">
+        <p class="footer_text">&copy; WhoAsked 2023</p>
+    </div>
+</footer>
+<html>
+<?php
+} else {
+?>
+<footer>
+    <div class="footer_container">
+        <div class="footer-left">
+            <img src="logo_white_small.png" class="logo">
+            <p class="footer_text">Communicate with others through our forum!</p>
+        </div>
+        <ul class="footer-right">
+            <li>
+                <h2 class="footer-title">Explore</h2>
+                <ul class="explore">
+                    <li><a href="popular.php" class="footer_text">Popular</a></li>
+                    <li><a href="topics.php" class="footer_text">Topics</a></li>
+                </ul>
+            </li>
+
+            <li>
+                <h2 class="footer-title">Legal</h2>
+                <ul class="legal">
+                    <li><a href="privacy.php" class="footer_text">Privacy Policy</a></li>
                 </ul>
             </li>
 
             <li>
                 <h2 class="footer-title">Get Started</h2>
                 <ul class="start">
-                    <li><a href="login.php">Login</a></li>
-                    <li><a href="register.php">Register</a></li>
+                    <li><a href="login.php" class="footer_text">Login</a></li>
+                    <li><a href="register.php" class="footer_text">Register</a></li>
                 </ul>
             </li>
 
@@ -83,20 +80,20 @@
                 <ul class="about">
                     <li>+31 (0)20 525 1401 </li>
                     <li>
-                        <p>Science Park 904 1098 XH Amsterdam</p>
+                        <p class="footer_text">Science Park 904 1098 XH Amsterdam</p>
                     </li>
                     <li>
-                        <p>servicedesk-fs@uva.nl</p>
+                        <p class="footer_text">servicedesk-fs@uva.nl</p>
                     </li>
             </li>
         </ul>
     </div>
     <div class="footer-bottom">
-        <p>&copy; WhoAsked 2023</p>
+        <p class="footer_text">&copy; WhoAsked 2023</p>
     </div>
 </footer>
 <html>
 <?php
-                    }
-                ?>
+}
+?>
 <html>
--- index.php
+++ index.php
@@ -1,60 +1,59 @@
 <?php
-    include "header.php";
-    include "../../connection.php";
-?>
+include "header.php";
+include "../../connection.php";
 
-<?php
 if (array_key_exists("login_user", $_SESSION)) {
     $userId = $_SESSION['user_id'];
 
     $sql = "SELECT * FROM `Posts` JOIN `Users` u on Posts.user = u.ID";
     $result = mysqli_query($connection, $sql);
-
 ?>
-
 <body>
-    <br>
-    <h1>Latest posts</h1>
-    <div class="posts">
-        <?php 
-while ($posts = $result->fetch_assoc()) { ?>
-        <div class="post_info">
-            <div class="container">
-                <a href="user_post.php?id=<?php echo $posts['postid']; ?>&username=<?php echo $posts['user']; ?>">
-                    <h2><?php echo $posts['title'] ?></h2>
-                    <p><?php echo $posts['contents'] ?></p>
-                    <p><?php echo $posts['category'] ?></p>
-                    <p>By <?php echo $posts['user'] ?></p>
-                    <div class="extra" style="position:relative;">
-                        <span> <?php echo $posts['likes'] ?> Likes | Published on:
-                            <?php echo $posts['datetime'] ?></span>
+    <div class="index_container">
+        <div class="index_main">
+            <br>
+            <h1>Latest posts</h1>
+            <div class="posts">
+                <?php
+while ($posts = $result->fetch_assoc()) {?>
+                <div class="post_info">
+                    <div class="post_container">
+                        <a
+                            href="user_post.php?id=<?php echo $posts['postid']; ?>&username=<?php echo $posts['user']; ?>">
+                            <h2><?php echo $posts['title'] ?></h2>
+                            <p><?php echo $posts['contents'] ?></p>
+                            <p><?php echo $posts['category'] ?></p>
+                            <p>By <?php echo $posts['user'] ?></p>
+                            <div class="extra" style="position:relative;">
+                                <span> <?php echo $posts['likes'] ?> Likes | Published on:
+                                    <?php echo $posts['datetime'] ?></span>
+                            </div>
+                        </a>
                     </div>
-                </a>
-            </div>
-        </div>
+                </div>
 </body>
-<?php }
-?>
-</div>
-<?php
-} else {    
-?>
-
-<div class="mainpage">
-    <div class="txt">
-        <h1 class="welcome">Welcome to<br>WhoAsked!</h1>
-        <h2 class="sign">Sign up now!</h2>
-    </div>
-    <div class="HomeImage">
-        <img src="interaction.png" alt="interaction" class="interaction" width="550" height="550">
-    </div>
-</div>
-
 <?php 
 } 
 ?>
-
+</div>
+</div>
+</div>
 <?php
-    include "footer.php";
-    include "cookiespopup.php";
+} else {
+?>
+<div class="index_container">
+    <div class="welcome_main">
+        <div class="txt">
+            <h1 class="welcome">Welcome to<br>WhoAsked!</h1>
+            <h2 class="sign">Sign up now!</h2>
+        </div>
+        <div class="HomeImage">
+            <img src="interaction.png" alt="interaction" class="interaction" width="550" height="550">
+        </div>
+    </div>
+</div>
+<?php
+}
+include "footer.php";
+include "cookiespopup.php";
 ?>
