650d2ce1dc1ac4c0d03beb13c4ed0c563e0ec3e3
Edwin <edwincheng619@gmail.com>
2023-02-01 21:52:31 +0100

    log in automatically after registering

--- add_user.php
+++ add_user.php
@@ -1,5 +1,6 @@
 <?php
-include '../../connection.php';
+include "../../connection.php";
+session_start();
 
 $name = mysqli_real_escape_string($connection, htmlspecialchars($_POST["name"]));
 $email = mysqli_real_escape_string($connection, htmlspecialchars($_POST["email"]));
@@ -7,12 +8,24 @@ $user = mysqli_real_escape_string($connection, htmlspecialchars($_POST["username
 $pass = mysqli_real_escape_string($connection, htmlspecialchars($_POST["password"]));
 $hash = password_hash($pass, PASSWORD_DEFAULT);
 
-
 try {
-$sql = "INSERT INTO Users (name, user, email, pass) VALUES ('$name', '$user', '$email', '$hash')";
-mysqli_query($connection, $sql);
-} catch(PDOExeption $e) {
+    $sql = "INSERT INTO Users (name, user, email, pass) VALUES ('$name', '$user', '$email', '$hash')";
+    mysqli_query($connection, $sql);
+
+    $username = mysqli_real_escape_string($connection, $_POST['username']);
+    $mypassword = mysqli_real_escape_string($connection, $_POST['password']);
+    $login = "SELECT * FROM Users WHERE user = '$username'";
+    $result = mysqli_query($connection, $login);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+    if ($row) {
+        if (password_verify($mypassword, $row["pass"])) {
+            $_SESSION['login_user'] = $username;
+            $_SESSION['user_id'] = $row["ID"];
+            header("location: index.php");
+        }
+    }
+
+} catch (PDOExeption $e) {
     echo $sql . "<br>" . $e->getMessage();
 }
-header("location: index.php");
-?>
